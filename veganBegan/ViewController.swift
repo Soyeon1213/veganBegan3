38f796f785e91f1684c69e58b480a0acb22a8cfe
Yoon-Jo Hwang <mymelody112266@gmail.com>
2021-05-05 14:38:55 +0900

    Initial Commit

---
+++ veganBegan/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  veganBegan
+//
+//  Created by Release on 2021/05/05.
+//  Copyright © 2021 Release. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

73386706cb69881e08d871cf863423d9c38eba9b
Hyunsu Moh <hyunsumo@gmail.com>
2021-06-02 04:19:50 +0900

    Create MapViewController

---
+++ veganBegan/MapViewController.swift
@@ -0,0 +1,60 @@
+//
+//  MapViewController.swift
+//  veganBegan
+//
+//  Created by RelMac User Exercise3 on 2021/06/02.
+//  Copyright © 2021 Release. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController, MTMapViewDelegate {
+    var mapView: MTMapView?
+    var mapPoint: MTMapPoint?
+    var poiItem1: MTMapPOIItem?
+    // test
+    var DBop: DatabaseManager = DatabaseManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+        mapView = MTMapView(frame: self.view.bounds)
+        
+        if let mapView = mapView{
+            mapView.delegate = self
+            mapView.baseMapType = .standard
+            
+            mapView.setMapCenter(MTMapPoint(geoCoord: MTMapPointGeo(latitude:37.573083, longitude: 126.983168)), zoomLevel: 4, animated: true)
+           
+            mapView.showCurrentLocationMarker = true
+            mapView.currentLocationTrackingMode = .onWithoutHeading
+            
+            self.mapPoint = MTMapPoint(geoCoord: MTMapPointGeo(latitude:37.573883, longitude: 126.983168))
+            
+            poiItem1 = MTMapPOIItem()
+            poiItem1?.markerType = MTMapPOIItemMarkerType.redPin
+            poiItem1?.mapPoint = mapPoint
+            poiItem1?.itemName = "veganBegan!"
+           
+            mapView.add(poiItem1)
+       
+            self.view.addSubview(mapView)
+            
+            // test
+            DBop.test()
+        }
+    }
+    
+    func mapView(_ mapView: MTMapView!, updateCurrentLocation location: MTMapPoint!, withAccuracy accuracy: MTMapLocationAccuracy){
+        let currentLocation = location?.mapPointGeo()
+        if let latitude = currentLocation?.latitude, let longitude = currentLocation?.longitude{ print("MTMapView update Current Location (\(latitude), \(longitude))")}
+            
+        }
+    
+    func mapView(_ mapView: MTMapView?, updateDeviceHeading headingAngle: MTMapRotationAngle) {
+        print("MTMapView update Device Heading (\(headingAngle)) degrees")
+    }
+}
